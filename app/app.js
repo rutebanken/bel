a0a64eae6a814227c882bbc32c277a6f2857c187
Tommy Gagnes <tg@scienta.no>
2016-01-28 21:26:50 +0100

    POC-184 Submitting uploaded files to REST service

--- app/app.js
+++ app/app.js
@@ -3,7 +3,6 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
-  'operationalStatusServices',
   'myApp.view1',
   'myApp.view2',
   'myApp.version'
--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute', 'operationalStatusServices'])
+angular.module('myApp.view1', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
@@ -9,39 +9,32 @@ angular.module('myApp.view1', ['ngRoute', 'operationalStatusServices'])
   });
 }])
 
-.controller('View1Ctrl', ['$scope', '$http', 'OpStatus', 'FileUpload', function($scope, $http, OpStatus, FileUpload) {
-    // $scope.$log = $log;
+.controller('View1Ctrl', ['$scope', '$http', function($scope, $http) {
+    $scope.providerId = 2
 
     $scope.add = function(){
+      var file = document.getElementById('file').files[0];
+      var backendUrl = 'http://localhost:8080/opstatus/' + $scope.providerId + '/uploadFile';
+      console.log("Uploading file with name '" + file.name + "' to '" + backendUrl + "'.");
 
-    var file = document.getElementById('file').files[0];
-          // r = new FileReader();
+      var fd = new FormData();
+      fd.append('file', file, file.name);
+      console.log("Posting to server");
+      $http.post(backendUrl, fd, {
+        // this cancels AngularJS normal serialization of request
+        transformRequest: angular.identity,
+        // this lets browser set `Content-Type: multipart/form-data`
+        // header and proper data boundary
+        headers: {'Content-Type': undefined}
+        // headers: {'Content-Type':'multipart/form-data'}
+      })
 
-    console.log("Received file with name " + file.name)
+      .success(function(){
+        console.log("Success")
+      })
 
-    var backendUrl = 'http://localhost:8080/opstatus/uploadFile';
-
-    var fd = new FormData();
-    fd.append('file', file, file.name);
-    fd.append('providerId', '2');
-
-    console.log("Posting to server");
-
-    $http.post(backendUrl, fd, {
-    // this cancels AngularJS normal serialization of request
-    transformRequest: angular.identity,
-    // this lets browser set `Content-Type: multipart/form-data`
-    // header and proper data boundary
-    headers: {'Content-Type': undefined}
-    // headers: {'Content-Type':'multipart/form-data'}
-})
-
-.success(function(){
-  console.log("Success")
-})
-
-.error(function(){
-    console.log("Something went wrong")
-});
+      .error(function(){
+        console.log("Something went wrong")
+      });
 
 }}])
