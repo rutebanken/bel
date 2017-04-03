89ec9d64d69723c98842d6735ec1e86e92682e0e
Morten Friesgaard <friesgaard@gmail.com>
2017-04-03 13:15:37 +0200

    does not exclude node_modules from assets loading

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -48,7 +48,6 @@ module.exports = {
       },
       {
         test: /\.(jpe?g|png|gif|svg)$/i,
-        exclude: /node_modules/,
         loaders: [
           'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
           'image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false'
