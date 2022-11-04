1258145060e9dda48356f20aa82723034d299e39
Tom Erik Støwer <testower@gmail.com>
2022-11-04 09:10:38 +0100

    Add temporary hostnames to config

--- src/config/getEnvironment.ts
+++ src/config/getEnvironment.ts
@@ -1,7 +1,7 @@
 export const getEnvironment = () => {
-  if (window.location.hostname === "operator.entur.org") {
+  if (window.location.hostname === "operator.entur.org" || window.location.hostname === "ent-bel-prd.web.app") {
     return "prod";
-  } else if (window.location.hostname === "operator.staging.entur.org") {
+  } else if (window.location.hostname === "operator.staging.entur.org" || window.location.hostname === "ent-bel-tst.web.app") {
     return "test";
   } else {
     return "dev";
